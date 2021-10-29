e92ac21fa4046b4d6464ac21127ea96db9e90afc
Rusty DeGarmo <rustydegarmo@yahoo.com>
2021-10-29 12:07:32 -0500

    module 3, a little about me

---
+++ module-3/script.js
@@ -0,0 +1,23 @@
+function firstFunction(){
+    let sentenceOne = "Then one day I had an epiphany";
+
+    document.getElementById('first').innerHTML = sentenceOne
+}
+
+function secondFunction(){
+    let sentenceTwo = "So, I opened a second bank account and started putting $200 a month in it just for fuel";
+
+    document.getElementById('second').innerHTML = sentenceTwo
+}
+
+function thirdFunction(){
+    let sentenceThree = "I noticed that I wasn't spending all of my fuel money and a little money was actually building up in this second account";
+
+    document.getElementById('third').innerHTML = sentenceThree
+}
+
+function fourthFunction(){
+    let sentenceFour = "It may be a lot easier to save money with a higher income, but saving isn't based on income it's based on mindset (scarcity) and behavior";
+
+    document.getElementById('fourth').innerHTML = sentenceFour
+}
